f6fa2c1011e42279d1f536c72ce87e6462f3e674
bavly-ashraf <122296672+bavly-ashraf@users.noreply.github.com>
2023-03-14 18:00:48 +0200

    handled an error

--- src/Pages/Cart/cart.js
+++ src/Pages/Cart/cart.js
@@ -5,18 +5,27 @@ export default function Cart() {
 
   useEffect(() => {
     const itemArr = JSON.parse(localStorage.getItem('cartItemArr'));
-    if ((itemArr === null || itemArr === [null]) && (JSON.parse(localStorage.getItem('items')) === null || JSON.parse(localStorage.getItem('items')) === [null])){
+    if ((itemArr == null || itemArr === [null])){
       console.log('quit successfully!');
     }
-    else {
-    localStorage.setItem('items', JSON.stringify([itemArr, JSON.parse(localStorage.getItem('items'))]));
-    console.log(JSON.parse(localStorage.getItem('items')));
-    console.log('Set Successfully');
+    else if (JSON.parse(localStorage.getItem('items')) == null || JSON.parse(localStorage.getItem('items')) === [null]) {
+      localStorage.setItem('items', JSON.stringify(itemArr));
+      console.log(JSON.parse(localStorage.getItem('items')));
+      console.log('Set Successfully 1');
     }
+    else if (itemArr === JSON.parse(localStorage.getItem('cartItemArr'))){
+    console.log(JSON.parse(localStorage.getItem('items')));
+    console.log('Set Successfully 2');
+    }
+    else {
+      localStorage.setItem('items', JSON.stringify([itemArr, JSON.parse(localStorage.getItem('items'))]));
+      console.log(JSON.parse(localStorage.getItem('items')));
+      console.log('Set Successfully 3');
+      }
 });
 
 const CartArr = () => {
-if (JSON.parse(localStorage.getItem('items')) === (null || [null]))
+if (JSON.parse(localStorage.getItem('items')) == null || JSON.parse(localStorage.getItem('items')) === [null])
 {
 return <h2>No items in the cart yet</h2>
 }
